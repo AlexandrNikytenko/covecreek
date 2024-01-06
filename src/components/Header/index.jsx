ad2aff05db546673a9fcfd4ab925fedb55b9af37
zdb <zdbrwn@gmail.com>
2024-01-05 17:26:33 -0700

    revert

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -29,128 +29,126 @@ function Header() {
   }, [prevScrollPos]);
 
   return (
-    <>
+    <div
+      className={`${styles.container} ${!isMenuOpen ? styles.menuOpen : ""}`}
+    >
       {!isPrivacy && <div className={styles.shadow}></div>}
-
-      <div
-        className={`${styles.container} ${!isMenuOpen ? styles.menuOpen : ""}`}
-        style={{
-          top: isHeaderVisible ? "0px" : "-100px",
-        }}
-      >
-        {!isMenuOpen && (
-          <div className={styles.head}>
+      {!isMenuOpen && (
+        <div
+          className={styles.head}
+          style={{
+            top: isHeaderVisible ? "0px" : "-100px",
+            backgroundColor:
+              prevScrollPos === 0 ? "transparent" : "rgb(9, 19, 29)",
+          }}
+        >
+          <Link
+            to="/"
+            className={`${styles.linked} ${
+              !isPrivacy ? styles.linked__no_privacy : styles.linked__privacy
+            }`}
+          ></Link>
+          <div className={styles.buttons}>
             <Link
-              to="/"
-              className={`${styles.linked} ${
-                !isPrivacy ? styles.linked__no_privacy : styles.linked__privacy
+              to={PAGES.Contact.path}
+              className={`${styles.button_get} ${
+                !isPrivacy
+                  ? styles.button_get__no_privacy
+                  : styles.button_get__privacy
               }`}
-            ></Link>
-            <div className={styles.buttons}>
-              <Link
-                to={PAGES.Contact.path}
-                className={`${styles.button_get} ${
-                  !isPrivacy
-                    ? styles.button_get__no_privacy
-                    : styles.button_get__privacy
-                }`}
-              >
-                Get in touch
-              </Link>
-              <button type="button" onClick={() => setIsMenuOpen(true)}>
-                <img
-                  src="/icons/Burger_close.svg"
-                  alt="Menu close"
-                  className={styles.burger}
-                />
-              </button>
-            </div>
+            >
+              Get in touch
+            </Link>
+            <button type="button" onClick={() => setIsMenuOpen(true)}>
+              <img
+                src="/icons/Burger_close.svg"
+                alt="Menu close"
+                className={styles.burger}
+              />
+            </button>
           </div>
-        )}
+        </div>
+      )}
 
-        <div className={styles.menu}>
-          <div className={styles.shadow}></div>
-          {isMenuOpen && (
-            <>
-              <div className={styles.head_menu}>
-                <Link
-                  to="/"
-                  onClick={() => setIsMenuOpen(false)}
-                  className={`${styles.linked} ${styles.linked__no_privacy}`}
-                ></Link>
-                <div className={styles.buttons}>
-                  <Link
-                    to={PAGES.Contact.path}
-                    onClick={() => setIsMenuOpen(false)}
-                    className={`${styles.button_get} ${styles.button_get__no_privacy}`}
-                  >
-                    Get in touch
-                  </Link>
-                  <button type="button" onClick={() => setIsMenuOpen(false)}>
-                    <img
-                      src="/icons/Burger_open.svg"
-                      alt="Menu open"
-                      className={styles.burger}
-                    />
-                  </button>
-                </div>
-              </div>
-              <div
-                className={styles.links}
+      <div className={styles.menu}>
+        <div className={styles.shadow}></div>
+        {isMenuOpen && (
+          <>
+            <div className={styles.head_menu}>
+              <Link
+                to="/"
                 onClick={() => setIsMenuOpen(false)}
-              >
-                <Link
-                  to={PAGES.Portfolio.path}
-                  className={`${styles.link} ${styles.link__underline}`}
-                >
-                  Portfolio
-                </Link>
-                <Link
-                  to={PAGES.WhyUs.path}
-                  className={`${styles.link} ${styles.link__underline}`}
-                >
-                  Why Us
-                </Link>
-                <Link
-                  to={PAGES.CaseStudy.path}
-                  className={`${styles.link} ${styles.link__underline}`}
-                >
-                  Case Study
-                </Link>
-                <Link
-                  to={PAGES.Careers.path}
-                  className={`${styles.link} ${styles.link__underline}`}
-                >
-                  Careers
-                </Link>
+                className={`${styles.linked} ${styles.linked__no_privacy}`}
+              ></Link>
+              <div className={styles.buttons}>
                 <Link
                   to={PAGES.Contact.path}
-                  className={`${styles.link} ${styles.link__underline}`}
+                  onClick={() => setIsMenuOpen(false)}
+                  className={`${styles.button_get} ${styles.button_get__no_privacy}`}
                 >
-                  Contact
+                  Get in touch
                 </Link>
+                <button type="button" onClick={() => setIsMenuOpen(false)}>
+                  <img
+                    src="/icons/Burger_open.svg"
+                    alt="Menu open"
+                    className={styles.burger}
+                  />
+                </button>
               </div>
-              <div className={styles.contacts}>
-                <a href="tel:+12137708211" className={styles.contacts__link}>
-                  213-770-8211
-                </a>
-                <div className={styles.contacts__devider}></div>
-                <a
-                  href="mailto:info@covecreekproductions.com"
-                  className={styles.contacts__link}
-                >
-                  info@covecreekproductions.com
-                </a>
-                <div className={styles.contacts__devider}></div>
-                <a className={styles.contacts__link}>
-                  2390 E Camelback Rd, Phoenix, AZ 85016
-                </a>
-              </div>
-            </>
-          )}
-        </div>
+            </div>
+            <div className={styles.links} onClick={() => setIsMenuOpen(false)}>
+              <Link
+                to={PAGES.Portfolio.path}
+                className={`${styles.link} ${styles.link__underline}`}
+              >
+                Portfolio
+              </Link>
+              <Link
+                to={PAGES.WhyUs.path}
+                className={`${styles.link} ${styles.link__underline}`}
+              >
+                Why Us
+              </Link>
+              <Link
+                to={PAGES.CaseStudy.path}
+                className={`${styles.link} ${styles.link__underline}`}
+              >
+                Case Study
+              </Link>
+              <Link
+                to={PAGES.Careers.path}
+                className={`${styles.link} ${styles.link__underline}`}
+              >
+                Careers
+              </Link>
+              <Link
+                to={PAGES.Contact.path}
+                className={`${styles.link} ${styles.link__underline}`}
+              >
+                Contact
+              </Link>
+            </div>
+            <div className={styles.contacts}>
+              <a href="tel:+12137708211" className={styles.contacts__link}>
+                213-770-8211
+              </a>
+              <div className={styles.contacts__devider}></div>
+              <a
+                href="mailto:info@covecreekproductions.com"
+                className={styles.contacts__link}
+              >
+                info@covecreekproductions.com
+              </a>
+              <div className={styles.contacts__devider}></div>
+              <a className={styles.contacts__link}>
+                2390 E Camelback Rd, Phoenix, AZ 85016
+              </a>
+            </div>
+          </>
+        )}
       </div>
-    </>
+    </div>
   );
 }
 
