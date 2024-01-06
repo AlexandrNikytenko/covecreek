b3962f314cbb1df6f83273e9f748457e7e6abfb0
zdb <zdbrwn@gmail.com>
2024-01-05 20:56:06 -0700

    increase slider speed and interval

--- src/components/University/index.jsx
+++ src/components/University/index.jsx
@@ -1,7 +1,6 @@
 import { motion, useAnimation } from "framer-motion";
 import { useEffect } from "react";
 import { useInView } from "react-intersection-observer";
-
 import { UniversityCarousel } from "../Carousel";
 import ArticleComponent from "../ArticleComponent";
 import RobustComponent from "../RobustComponent";
